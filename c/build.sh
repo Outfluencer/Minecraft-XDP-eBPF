de497f0779ada7386eaddba6cef66a3e1e4aecae
Outfluencer <git@outfluencer.dev>
2025-06-29 16:07:21 +0200

    Add  block_ip and connection_throttle option to c/build.sh for better visibility

--- c/build.sh
+++ c/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
-clang -DSTART_PORT=25000 -DEND_PORT=26000 -Wall -Wextra -Wno-language-extension-token -O3 -g -target bpf -mcpu=v3 -c "minecraft_filter.c" -o "minecraft_filter.o"
+clang -DBLOCK_IPS=1 -DCONNECTION_THROTTLE=1 -DSTART_PORT=25000 -DEND_PORT=26000 -Wall -Wextra -Wno-language-extension-token -O3 -g -target bpf -mcpu=v3 -c "minecraft_filter.c" -o "minecraft_filter.o"
