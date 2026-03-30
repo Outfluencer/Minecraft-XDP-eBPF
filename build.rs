c09bd0bc9dfd359a441db55ea430fbd9a2af1579
Outfluencer <git@outfluencer.dev>
2026-03-30 13:11:44 +0200

    Revert adding'-mattr=+alu32' to build flags

--- build.rs
+++ build.rs
@@ -54,7 +54,6 @@ fn main() {
         "-g",
         "-target", "bpf",
         "-mcpu=v3",
-        "-mattr=+alu32",
         "-c", "minecraft_filter.c",
         "-o", "minecraft_filter.o",
     ]);
